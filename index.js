531b5151684cfa87f0ef4cb8ed33a4c002379d46
IamArnab01 <imarnab01@gmail.com>
2022-01-25 21:45:18 +0530

    first commit -> server started

---
+++ index.js
@@ -0,0 +1,23 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const dotenv = require("dotenv");
+const cors = require("cors");
+
+// spinnig the express app
+const app = express();
+// dotenv config
+dotenv.config();
+
+const PORT = process.env.PORT || 5000;
+
+// connecting to database
+mongoose.connect(process.env.DB_URL, { useNewUrlParser: true }, () =>
+  console.log("db connected")
+);
+
+// middlewares
+app.use(cors());
+app.use(express.json());
+// route middlewares
+
+app.listen(PORT, () => console.log(`App Runnig on port ${PORT}`));
