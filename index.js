a11b86097b02aeae7109cfd8e42bb325373e9285
IamArnab01 <imarnab01@gmail.com>
2022-01-27 21:14:45 +0530

    modify

--- index.js
+++ index.js
@@ -2,6 +2,7 @@ const express = require("express");
 const mongoose = require("mongoose");
 const dotenv = require("dotenv");
 const cors = require("cors");
+const path = require("path");
 const authRoutes = require("./routes/auth");
 const profileRoutes = require("./routes/profile");
 const chatUserPairsRoutes = require("./routes/chatUserPairs");
